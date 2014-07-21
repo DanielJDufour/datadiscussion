2a50ed54b7d17632f6ba432414c3399d9b2bd9b3
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 00:58:05 -0400

    Updated Dataset Model

--- datadiscussion/dataportal/models.py
+++ datadiscussion/dataportal/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-class DataSet(models.Model):
-  title = models.CharField(max_length=200)
-  source = models.CharField(max_length=200)
-  strengths = models.CharField(max_length=200)
-  weaknesses = models.CharField(max_length=200)
+class Dataset(models.Model):
+  title_en = models.CharField(max_length=200)
+  source_en = models.CharField(max_length=200)
+  hyperlink_en = models.URLField(max_length=200)
+  theme_en = models.CharField(max_length=200)
+  format_en = models.CharField(max_length=200)
+  frequency = models.CharField(max_length=200)
+  strengths_en = models.CharField(max_length=200)
+  weaknesses_en = models.CharField(max_length=200)
+  downloadable = models.NullBooleanField()
+  last_updated = models.DateTimeField()
