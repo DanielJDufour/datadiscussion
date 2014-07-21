f1a20ddb685e5e7b2bfb7160a1fe812e839a8ef7
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 02:10:24 -0400

    C strengths and weaknesses to TextField

--- datadiscussion/dataportal/models.py
+++ datadiscussion/dataportal/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Dataset(models.Model):
-  title_en = models.CharField(max_length=200)
-  source_en = models.CharField(max_length=200)
-  hyperlink_en = models.URLField(max_length=200)
-  location_en = models.CharField(max_length=200)
-  location_shp = models.URLField(max_length=200)
-  theme_en = models.CharField(max_length=200)
-  type_en = models.CharField(max_length=200)
-  frequency = models.CharField(max_length=200)
-  strengths_en = models.CharField(max_length=200)
-  weaknesses_en = models.CharField(max_length=200)
+  title_in_english = models.CharField(max_length=200)
+  source_in_english = models.CharField(max_length=200)
+  hyperlink_in_english = models.URLField(max_length=200)
+  location_in_english = models.CharField(max_length=200)
+  location_in_english = models.URLField(max_length=200)
+  theme_in_english = models.CharField(max_length=200)
+  format_in_english = models.CharField(max_length=200)
+  frequency_in_english = models.TextField()
+  strengths_in_english = models.TextField()
+  weaknesses_in_english = models.CharField(max_length=200)
   downloadable = models.NullBooleanField()
   last_updated = models.DateTimeField()
   def __str__(self):
