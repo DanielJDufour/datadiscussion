8abcfd13d2b8540105d944e903d591c6e1820673
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 17:11:58 -0400

    + dataportal.urls

--- datadiscussion/datadiscussion/urls.py
+++ datadiscussion/datadiscussion/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'datadiscussion.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^dataportal/', include('dataportal.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
