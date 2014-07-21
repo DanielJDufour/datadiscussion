3c01b6b2f9bfd0cb0664361e1ad65e0ab2cc7f08
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 17:19:15 -0400

    + view submit

--- datadiscussion/dataportal/views.py
+++ datadiscussion/dataportal/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("Hello.  You're at the data discussion index.")
+
+def view(request):
+    return HttpResponse("Hello.  You're at the view data page.")
+
+def submit(request):
+    return HttpResponse("Hello.  You're at the submit data page.")
