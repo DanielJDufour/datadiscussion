76d5c9ca0d69bc518c3d9012914c02c39799dae3
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 02:24:29 -0400

    Update admin.py

--- datadiscussion/dataportal/admin.py
+++ datadiscussion/dataportal/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from dataportal.models import Dataset, Twitter, Facebook, GIS
 
 admin.site.register(Dataset)
-admin.site.register(Twitter, Facebook, GIS)
+admin.site.register(Twitter)
+admin.site.register(Facebook)
+admin.site.register(GIS)
+
