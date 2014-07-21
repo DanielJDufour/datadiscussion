b30aab70e714393faecc3499911408e2605fe064
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 02:16:46 -0400

    + import for Twitter, Facebook and GIS

--- datadiscussion/dataportal/admin.py
+++ datadiscussion/dataportal/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from dataportal.models import Dataset
+from dataportal.models import Twitter
+from dataportal.models import Facebook
+from dataportal.models import GIS
+
 
 admin.site.register(Dataset)
 admin.site.register(Twitter)
