fefd840762443940471f5b7d87e5c6bf06b00679
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-22 17:09:49 -0400

    import SiteInfo

--- datadiscussion/dataportal/views.py
+++ datadiscussion/dataportal/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader, Context
+from dataportal.models import SiteInfo
 
 
 def index(request):
