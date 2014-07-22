9a356b7f75086e1feee184b097541c1587b689a5
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-21 23:29:45 -0400

    Update views.py

--- datadiscussion/dataportal/views.py
+++ datadiscussion/dataportal/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+
 def index(request):
-  template = loader.get_template('dataportal/index.html')
-  return HttpResponse(template.render(request))
+    context = {}
+    return render(request, 'dataportal/index.html', context)
 
 def view(request):
   return HttpResponse("Hello.  You're at the view data page.")
