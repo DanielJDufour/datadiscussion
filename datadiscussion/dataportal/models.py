bae53468d5d13653eaac63018cc8697712290a4f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-23 17:41:32 -0400

    Update models.py

--- datadiscussion/dataportal/models.py
+++ datadiscussion/dataportal/models.py
@@ -1,6 +1,6 @@
+from django import forms
 from django.db import models
 from django.contrib.auth.models import User
-
 
 class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
