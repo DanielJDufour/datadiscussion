9e42920d9a3e7c08e6139093955cd2d8c882ff4b
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-23 18:23:07 -0400

    Update urls.py

--- datadiscussion/dataportal/urls.py
+++ datadiscussion/dataportal/urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls import url
+    from django.conf.urls import patterns, include, url
+    from dataportal.views import *
+
+
 
 from dataportal import views
 
@@ -10,4 +14,10 @@ urlpatterns = [
     url('view', views.view, name='view'),
     url('submit', views.submit, name='submit'),
     url('register', views.register, name='register'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+)
+    
 ]
