02e4c48fda78b3c915ffab957077bdbc5479d1d1
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-23 17:37:06 -0400

    Update models.py

--- datadiscussion/dataportal/models.py
+++ datadiscussion/dataportal/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, forms
 from django.contrib.auth.models import User
 
 
@@ -13,6 +13,18 @@ class UserProfile(models.Model):
     # Override the __unicode__() method to return out something meaningful!
     def __unicode__(self):
         return self.user.username
+        
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
 
 class SiteInfo(models.Model):
   topic = models.CharField(max_length=200)
