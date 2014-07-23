544d6faa4a89a27261fd3fc403a75702eb357804
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-23 16:52:55 -0400

    + accounts login logout

--- datadiscussion/dataportal/urls.py
+++ datadiscussion/dataportal/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import url
 
 from dataportal import views
 
+from django.contrib.auth.views import login, logout
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url('view', views.view, name='view'),
     url('submit', views.submit, name='submit'),
+    url(r'^accounts/login/$',  login),
+    url(r'^accounts/logout/$', logout),
 ]
