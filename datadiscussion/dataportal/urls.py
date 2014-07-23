7fc049f439b3587ba9179999b5de697d52953862
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-23 18:59:17 -0400

    Update urls.py

--- datadiscussion/dataportal/urls.py
+++ datadiscussion/dataportal/urls.py
@@ -12,10 +12,18 @@ from django.conf.urls import patterns, include, url
 from dataportal.views import *
 
 
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url('view', views.view, name='view'),
     url('submit', views.submit, name='submit'),
-    url('register', views.register, name='register'),
+    url('^register/', CreateView.as_view(
+            template_name='register.html',
+            form_class=UserCreationForm,
+            success_url='/'
+    )),
+    url('^accounts/', include('django.contrib.auth.urls')),
 ]
