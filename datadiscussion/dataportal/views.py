b1cccdb0a5893c441ef8deb0b7a7b3862585f18e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-24 11:32:18 -0400

    Update views.py

--- datadiscussion/dataportal/views.py
+++ datadiscussion/dataportal/views.py
@@ -32,7 +32,7 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'], email=form.cleaned_data['email'], password=form.cleaned_data['password1'])
+            user = User.objects.create_user(username=form.cleaned_data['username'], '123@example.com', password=form.cleaned_data['password1'])
             user.first_name = "john"
             user.last_name = "doe"
             return HttpResponseRedirect('dataportal')
