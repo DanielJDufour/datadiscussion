2a4f0a22e468e932a3958dbf44678aef6b1ddf2b
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-07-24 11:16:21 -0400

    Update models.py

--- datadiscussion/dataportal/models.py
+++ datadiscussion/dataportal/models.py
@@ -1,30 +1,4 @@
-from django import forms
 from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
-
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    # Override the __unicode__() method to return out something meaningful!
-    def __unicode__(self):
-        return self.user.username
-        
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture')
 
 class SiteInfo(models.Model):
   topic = models.CharField(max_length=200)
